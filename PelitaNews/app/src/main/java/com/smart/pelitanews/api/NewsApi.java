0aea2680398fe9575fc38c2ccd3a6010ab07b405
Tri Purwanto <Tpurwanto12@gmail.com>
2020-07-09 23:04:43 +0700

    Project Kelompok PelitaNews

---
+++ PelitaNews/app/src/main/java/com/smart/pelitanews/api/NewsApi.java
@@ -0,0 +1,10 @@
+package com.smart.pelitanews.api;
+
+public class NewsApi {
+    public static String GET_TOP_HEADLINES = "http://newsapi.org/v2/top-headlines?country=id&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+    public static String GET_CATEGORY_SPORTS = "http://newsapi.org/v2/top-headlines?country=id&category=sports&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+    public static String GET_CATEGORY_TECHNOLOGY = "http://newsapi.org/v2/top-headlines?country=id&category=technology&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+    public static String GET_CATEGORY_BUSINESS = "http://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+    public static String GET_CATEGORY_HEALTH = "http://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+    public static String GET_CATEGORY_ENTERTAINMENT = "http://newsapi.org/v2/top-headlines?country=id&category=entertainment&apiKey=7c69d05e60114014a4fda46a3a62a07e";
+}
